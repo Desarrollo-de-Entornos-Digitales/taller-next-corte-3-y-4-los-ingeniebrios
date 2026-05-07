b0b1c1da44121d81676914a11eb1f0467ed48fc3
Sssebas81 <jsebastianortiz1881@gmail.com>
2026-05-06 22:45:15 -0500

    feat: components for login and register feature

---
+++ src/app/(public)/login/components/logincomponent.tsx
@@ -0,0 +1,6 @@
+
+    export default function logincomponent(){
+        return(
+            <h1>Hola</h1>
+        )
+    }
---
+++ src/app/(public)/register/components/registercomponent.tsx
@@ -0,0 +1,5 @@
+export default function registercomponent(){
+        return(
+            <h1>Hola</h1>
+        )
+    }
