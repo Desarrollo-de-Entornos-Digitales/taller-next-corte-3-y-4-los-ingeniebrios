3aa0d636e0cd1fd2b087579e1af70ddf9785c57a
Sssebas81 <jsebastianortiz1881@gmail.com>
2026-05-31 15:11:32 -0500

    feat: redirect to login if the user isnt login or try to enter in feed page

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+// src/app/(dashboard)/layout.tsx
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import Navbar from "../../common/components/Navbar";
+
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+    // 1. Leemos las cookies del navegador desde el servidor de Next.js
+    const cookiesStore = await cookies();
+    const token = cookiesStore.get("token")?.value;
+
+    // 2. 🔒 SI NO HAY TOKEN: Bloqueo inmediato.
+    // Si escribe /feed en la URL, Next.js frena la renderización aquí mismo y lo bota a /login
+    if (!token) {
+        redirect("/login");
+    }
+
+    // 3. Si sí hay token, se muestra la aplicación común y corriente
+    return (
+        <>
+            <Navbar title="Dashboard" />
+            <main className="min-h-screen bg-base-200">{children}</main>
+        </>
+    );
+}
