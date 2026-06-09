f95a1dd03340fbc2e69fefac416c0c27f7e5531b
Sara Grijalba <grijalbapsaras.cla@gmail.com>
2026-06-08 22:36:00 -0500

    file: add cypress test users complete

--- cypress/e2e/admin.spec.cy.ts
+++ cypress/e2e/admin.spec.cy.ts
@@ -7,11 +7,14 @@ describe("Admin Spec", () => {
     cy.get('[name="email"]').click();
     cy.get('[name="email"]').type('admin@u.icesi.edu.co');
     cy.get('[name="password"]').click();
+    cy.get('[name="password"]').click();
     cy.get('[name="password"]').type('password');
     cy.get('button.font-bold').click();
   });
  
-  it("should create a monitor", () => {});
+  it("should delete users ", () => {
+    
+  });
 
  
   it("should delete a  monitor", () => {});
--- cypress/e2e/monitorsspec.cy.ts
+++ cypress/e2e/monitorsspec.cy.ts
@@ -1,11 +1,11 @@
 describe("Monitor Spec", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3001");
+    cy.visit("http://localhost:3001/login");
   });
  
   it("should login as monitor", () => {});
  
-  it("should select carrer and semester", () => {});
+  it("should answer questions", () => {});
  
   it("should chat with a student", () => {});
  
